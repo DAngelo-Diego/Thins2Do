2d1a109a09bc6eac1f63e3544d825c77f42d27a8
DAngelo-Diego <dangelodiego967@gmail.com>
2022-08-15 10:59:52 -0300

    ToDo

---
+++ things2do/src/components/Task.tsx
@@ -0,0 +1,42 @@
+import "../stylesheets/Task.css";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+interface Props {
+  id: any;
+  text: any;
+  complete: any;
+  compTask: any;
+  deleteTask: any;
+  AiOutlineCloseCircle: any;
+}
+
+const Task: React.FC<Props> = ({
+  id,
+  text,
+  complete,
+  compTask,
+  deleteTask,
+  AiOutlineCloseCircle,
+}) => {
+  return (
+    <div className={complete ? "task-container completed" : "task-container"}>
+      <div className="task-text" onClick={() => compTask(id)}>
+        {text}
+      </div>
+      <div className="task-icon-container" onClick={() => deleteTask(id)}>
+        <AiOutlineCloseCircle className="task-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Task;
+
+// interface IMyProps {
+//   myValue: boolean;
+// }
+
+// const MyComponent: React.FC<IMyProps> = (props: IMyProps) => {
+//   //  ...
+// };
+
+// export default MyComponent;
